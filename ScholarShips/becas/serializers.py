1f89e0ffb810353b2eff87f0af8222bb4bbe1236
Indralil <ada_hack119@hackademy.mx>
2022-08-06 23:01:44 -0500

    primer migracion

---
+++ ScholarShips/becas/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Beca
+
+
+class BecaSerializer(serializers.Serializer):
+
+    class Meta:
+        model = Beca
+        fields = [
+            'id',
+            #'organizacion',
+            'nombrebeca',
+            'descripcion',
+        ]
+"""
+    def to_representation(self, instance):
+        
+        return {
+            'id': instance.id,
+            'organizacion': {
+                'id': instance.organizacion.id,
+                'name': instance.organizacion.name
+                },
+            'nombrebeca': instance.nombrebeca,
+            'descripcion': instance.descripcion
+        }
+"""
+
