8de25695a8dee89d6d3d6ecd498b9bdc22d62c38
Adriana <ada_hack88@hackademy.mx>
2022-08-07 16:39:56 -0500

    Staticfiles

--- ScholarShips/ScholarShips/settings/base.py
+++ ScholarShips/ScholarShips/settings/base.py
@@ -129,9 +129,16 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Simplified static file serving for Python web apps
+# https://whitenoise.evans.io/en/stable/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
