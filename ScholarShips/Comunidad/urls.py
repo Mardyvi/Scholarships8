4427ff638ed46af07b49690812f6d5e513473128
CeciAmaro <amaro.ceci.10@gmail.com>
2022-08-07 13:12:43 -0500

    Metodos para actualizar y eliminar

--- ScholarShips/Comunidad/urls.py
+++ ScholarShips/Comunidad/urls.py
@@ -5,6 +5,6 @@ from Comunidad.views import UpdateComunidad, DeleteComunidad
 urlpatterns = [
     path('', views.ComunidadView.as_view()),
     path('listar/', views.ListComunidad.as_view()),
-    #path('updateComunidad/<int:pk>/', views.UpdateComunidad.as_view(),),
+    path('updateComunidad/<int:pk>/', views.UpdateComunidad.as_view(),),
     path('deleteComunidad/<int:pk>/', views.DeleteComunidad.as_view(),),
 ]
