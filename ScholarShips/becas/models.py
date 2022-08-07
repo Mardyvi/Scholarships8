eb0933f1e0a2c0994da483b4d50d50d5c1b9e388
Indralil <ada_hack119@hackademy.mx>
2022-08-06 23:00:09 -0500

    configuracion app

--- ScholarShips/becas/models.py
+++ ScholarShips/becas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+#from organizaciones.models import Organizacion
 
 # Create your models here.
+
+class Beca(models.Model):
+
+    #organizacion = models.ForeignKey (Organizacion, on_delete=models.CASCADE)
+    nombrebeca = models.CharField (max_length=50)
+    descripcion = models.CharField (max_length=500)
+
+    class Meta:
+        db_table = 'becas'
+
+    def __str__(self):
+        return self.becas
--- ScholarShips/becas/views.py
+++ ScholarShips/becas/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Beca
+from .serializers import BecaSerializer
 
-# Create your views here.
+
+class BecaView(APIView):
+
+    def post(self, request):
+        serializer = BecaSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
